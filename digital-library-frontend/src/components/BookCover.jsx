1fbe0a89bae9340745c974ba151e9c393721b3c5
Keshavkumar9134 <keshavkr21022004@gmail.com>
2025-06-19 11:45:50 +0530

    Update BookCover.jsx

--- digital-library-frontend/src/components/BookCover.jsx
+++ digital-library-frontend/src/components/BookCover.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const BookCover = ({ coverImageUrl, title }) => (
   coverImageUrl && (
     <img
-      src={`http://localhost:5000/${coverImageUrl}`}
+      src={`https://digital-library-system-backend-hjqc.onrender.com/${coverImageUrl}`}
       alt={title}
       style={{ maxWidth: '100%', maxHeight: 400, marginBottom: '1rem' }}
     />
